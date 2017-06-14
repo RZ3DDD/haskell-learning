112489f4669523e7c553789a39d83f7b2f7faa45
YURY SHCHUTSKY <RZ3DDD@gmail.com>
2017-06-14 17:33:19 +0300

    Второе занятие, начала списков, 32стр.

--- baby.hs
+++ baby.hs
@@ -1,1 +1,13 @@
 doubleMe x = x + x
+
+doubleUs x y = x*2 + y*2
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                      then x
+                      else x + x
+
+doubleSmallNumber'  x = (if x > 100 then x else x + x) + 1
+doubleSmallNumber'' x =  if x > 100 then x else x + x + 1
+
+conanO'Brian = "Это я, Конан О'Брайан!"

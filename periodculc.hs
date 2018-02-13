9b669590cf28c0b7a31f83fbba90784e60942fe6
Yury Shchutsky <RZ3DDD@gmail.com>
2018-02-13 22:56:33 +0300

    окончательно 	изменено:      periodculc.hs

--- periodculc.hs
+++ periodculc.hs
@@ -6,43 +6,33 @@ infix 5 ~=
 (~=) :: Double -> Double -> Bool
 a ~= b = abs (a - b) < tolerance
 
--- Функция periodculc вычисляет количество точек (включая крайние) на заданом отрезке (размером lgth)
--- расположенными через равные промежутки (размером section).
--- Кроме lgth Необходимо задать maxsection и minsection, а также количество итераций (maxiter).
+-- Функция periodculc вычисляет количество точек (включая крайние) на заданом отрезке (размером lgth),
+-- расположенных через равные промежутки (размером section).
+-- Кроме lgth Необходимо задать maxsection и minsection.
 -- Вычисляет только нечетное количество точек, чтобы одна из точек была точно посредине отрезка. 
 -- Результат представляется в виде списка кортежей [(количество точек, размер промежутка)]. 
-periodculc :: Double -> Double -> Double -> Int -> [(Int, Double)]
-periodculc lgth maxsection minsection maxiter = 
-     filter pred (map (\ i -> (i, (fromIntegral (round (lgth/(fromIntegral (i-1)) * 10.0))) / 10.0 )) [3..maxiter])
-         where
-            pred :: (Int, Double) -> Bool
-            pred (n, section) = odd n
-                             && section <= maxsection
-                             && section >= minsection
-                             && lgth ~= section * fromIntegral (n-1)      
-                             
 
--- А в periodculc' количество итераций (maxiter) указывать не надо.
+-- Сначала такое нативное определение ...
 periodculc' :: Double -> Double -> Double -> [(Int, Double)]
 periodculc' lgth maxsection minsection = 
-     filter pred (reverse initialList)
+     filter pred ( map (\ i -> (i, (fromIntegral (round (lgth/(fromIntegral (i-1)) * 10.0))) / 10.0 ))
+                       [3.. round (lgth / minsection) + 1]
+                 )
          where
             pred :: (Int, Double) -> Bool
             pred (n, section) = odd n
                              && section <= maxsection
                              && lgth ~= section * fromIntegral (n-1)      
                              
-            initialList :: [(Int, Double)]
-            initialList =
-                until prd newTuple [(3, lgth / fromIntegral 2)]
-                   where 
-                      prd :: [(Int, Double)] -> Bool
-                      prd xs = snd (head xs) < minsection
-         
-                      newTuple :: [(Int, Double)] -> [(Int, Double)]
-                      newTuple xs = ( fst (head xs) + 1,
-                                      fromIntegral (round (lgth / fromIntegral (fst (head xs)) * 10.0)) / 10.0
-                                    ) : xs  
 
+-- И самое изящное (скажем так - математическое, посредством абстракции списка) определение сей функции ...
+periodculc :: Double -> Double -> Double -> [(Int, Double)]
+periodculc lgth maxsection minsection = 
+       [(n, section) | n <- [3 .. round (lgth / minsection) + 1] , 
+                                   let section = fromIntegral (round (lgth / fromIntegral (n-1) * 10.0)) / 10.0,
+                                   odd n,
+                                   section <= maxsection,
+                                   lgth ~= section * fromIntegral (n-1)
+       ]
 
 -- End of periodculc etude
